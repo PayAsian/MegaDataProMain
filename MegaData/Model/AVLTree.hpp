f333e06b0bceaf669c60a38a0e7669e256896e9e
PayAsian <PayAsian@yahoo.com>
2017-04-21 12:02:55 -0600

    ROTATIONS OF TREES

--- MegaData/Model/AVLTree.hpp
+++ MegaData/Model/AVLTree.hpp
@@ -26,7 +26,7 @@ private:
     BinarySearchTreeNode<Type> * insertNode(BinarySearchTreeNode<Type> * parent);
     BinarySearchTreeNode<Type> * removeNode(BinarySearchTreeNode<Type>  * parent);
     
-    it heightDifferencce(BinarySearchTreeNode<Type> * parent);
+    int heightDifferencce(BinarySearchTreeNode<Type> * parent);
     
 public:
     AVLTree();
@@ -35,4 +35,73 @@ public:
     void insert(Type itemToInsert);
     void remove(Type value);
 };
+
+/*
+ A negative value mean the right is greater than the left
+ */
+template<class Type>
+int AVLTree<Type> :: heightDiference(BinarySearchTreeNode<Type> * node)
+{
+    int balance;
+    int leftHeight = this->calculateHeight(node->getLeftChild());
+    int rightHeight = this->calculateHeight(node->getRightChild());
+    balance = leftHeight - rightHeight;
+    return balance;
+}
+
+template<class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: leftRotation(BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changeNode;
+    changedNode = parent->getLeftChild();
+    
+    parent->setLeftChild(changedNode->getRightChild());
+    
+    changedNode->setRightChild(parent);
+    
+    return changedNode;
+}
+
+template<class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: rightRotation(BinarySearchTreeNode<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getRightChild();
+    
+    parent->setRightChild(changedNode->getLeftChild());
+    
+    changedNode->setLeftChild(parent);
+    
+    return chagnedNode;
+}
+
+template<class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: rightLeftRotation(BinarySearchTree<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changedNode = parent->getRightChild();
+    
+    parent->setRightChild(leftRotation(changedNode));
+    
+    return rightRotation(parent);
+}
+
+template<class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: leftRightRotation(BinarySearchTree<Type> * parent)
+{
+    BinarySearchTreeNode<Type> * changedNode;
+    changeNode = parent->getLeftChild();
+    
+    parent->setLeftChild(rightRotation(changedNode));
+    
+    return leftRotation(parent);
+}
+
+template<class Type>
+BinarySearchTreeNode<Type> * AVLTree<Type> :: balancceSubTree(BinarySearchTreNode<Type> * parent)
+{
+    
+}
+
+
 #endif /* AVLTree_hpp */
