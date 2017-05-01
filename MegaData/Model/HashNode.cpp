5ef5f1b40023448a54ca12358eca7ecbd92e8024
PayAsian <PayAsian@yahoo.com>
2017-05-01 12:54:47 -0600

    Filling out data members in HashTable

---
+++ MegaData/Model/HashNode.cpp
@@ -0,0 +1,9 @@
+//
+//  HashNode.cpp
+//  MegaData
+//
+//  Created by Trevino, Peyton on 5/1/17.
+//  Copyright © 2017 Trevino, Peyton. All rights reserved.
+//
+
+#include <stdio.h>
--- MegaData/Model/HashTable.hpp
+++ MegaData/Model/HashTable.hpp
@@ -35,4 +35,87 @@ public:
     void displayContents();
 };
 
+template<class Type>
+HashTable<Type> :: HashTable()
+{
+    this->capacity = 101;
+    this->efficiencyPercentage = .667;
+    this->size = 0;
+    this->hashTableStorage = HashNode<Type> * [capacity];
+}
+
+template<class Type>
+HashTable<Type> :: ~HashTable()
+{
+    delete [] hashTableStorage;
+}
+
+template<class Type>
+int HashTable<Type> :: getNextPrime()
+{
+    int nextPrime = (this->capacity * 2) + 1;
+    
+    while(!isPrime(nextPrime))
+    {
+        nextPrime += 2;
+    }
+    
+    return nextPrime;
+}
+
+template<class Type>
+bool HashTable<Type> :: isPrime(long candidateNumber)
+{
+    if(candidateNumber <= 1)
+    {
+        return false;
+    }
+    else if(candidateNumber == 2 || canndidateNumber == 3)
+    {
+        return true;
+    }
+    else if(candidateNumber % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for(int next = 3; next <= sqrt(candidateNumber) +1; next += 2)
+        {
+            if(candidateNumber % next == 0)
+            {
+                return false
+            }
+        }
+        return false;
+    }
+}
+
+template<class Type>
+long HashTable<Type> :: findPosition(HashNode<Type> * data)
+{
+    long insertPosition = data->getKey() % this->capacity;
+    return insertPosition;
+}
+
+template<class Type>
+long HashTable<Type> :: handleCollision(HashNode<Type> * data, long currentPosition)
+{
+    long shift = 17;
+    
+    for(long position = currentPosition+ shift; position != currentPosition; position += shift)
+    {
+        if(position >= capacity)
+        {
+            position = position % capacity;
+        }
+        
+        if(hashTableStorage[position] == nullptr)
+        {
+            return position;
+        }
+    }
+    
+    return -1;
+}
 #endif /* HashTable_h */
