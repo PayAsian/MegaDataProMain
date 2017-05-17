91edf34df11cda3eca4173d86d810433ff6c7b5c
PayAsian <PayAsian@yahoo.com>
2017-05-17 13:16:27 -0600

    fixed alignment

--- MegaData/Controller/FileController.cpp
+++ MegaData/Controller/FileController.cpp
@@ -23,59 +23,59 @@ DoubleList<FoodItem> FileController :: readFoodItemDataFromFileAsList(string fil
         {
             getline(dataFile, currentCSVLine, '\r');
             stringstream parseCSV(currentCSVLine);
-                                  
-                                  string title, tempCost, tempCalorie, tempTasty;
-                                  
-                                  double cost;
-                                  int calories;
-                                  bool isDelish;
-                                  
-                                  getline(parseCSV, title, ',' );
-                                  getline(parseCSV, tempCost, ',');
-                                  getline(parseCSV, tempCalorie, ',');
-                                  getline(parseCSV, tempTasty, ',');
-                                  
-                                  if(rowCount != 0)
-                                  {
-                                      cost = stod(tempCost);
-                                      calories = stoi(tempCalorie);
-                                      isDelish = stoi(tempTasty);
-                                      
-                                      FoodItem temp(title);
-                                      temp.setCost(cost);
-                                      temp.setCalories(calories);
-                                      temp.setDelicious(isDelish);
-                                      
-                                      dataSource.add(temp);
-                                  }
-                                  rowCount++;
-                                  
-                                  cout << currentCSVLine << endl;
-                                  }
-                                  dataFile.close();
-                                  }
-                                  else
-                                  {
-                                      cerr << "NO FILE" << endl;
-                                  }
-                                  return dataSource;
-                                  }
-                                  
-                                  void FileController :: writeFoodItemDataStatistics(DoubleList<FoodItem> dataSource, string filename)
-                                  {
-                                      ofstream saveFile(filename);
-                                      
-                                      if(saveFile.is_open())
-                                      {
-                                          saveFile << "These are the contents of the list" << endl;
-                                          for(int index = 0; index < dataSource.getSize(); index ++)
-                                          {
-                                              saveFile << "Food Title: " << dataSource.getFromIndex(index).getFoodName() << endl;
-                                          }
-                                      }
-                                      else
-                                      {
-                                          cerr << "File unavailable" << endl;
-                                      }
-                                      saveFile.close();
-                                  }
+            
+            string title, tempCost, tempCalorie, tempTasty;
+            
+            double cost;
+            int calories;
+            bool isDelish;
+            
+            getline(parseCSV, title, ',' );
+            getline(parseCSV, tempCost, ',');
+            getline(parseCSV, tempCalorie, ',');
+            getline(parseCSV, tempTasty, ',');
+            
+            if(rowCount != 0)
+            {
+                cost = stod(tempCost);
+                calories = stoi(tempCalorie);
+                isDelish = stoi(tempTasty);
+                
+                FoodItem temp(title);
+                temp.setCost(cost);
+                temp.setCalories(calories);
+                temp.setDelicious(isDelish);
+                
+                dataSource.add(temp);
+            }
+            rowCount++;
+            
+            cout << currentCSVLine << endl;
+        }
+        dataFile.close();
+    }
+    else
+    {
+        cerr << "NO FILE" << endl;
+    }
+    return dataSource;
+}
+
+void FileController :: writeFoodItemDataStatistics(DoubleList<FoodItem> dataSource, string filename)
+{
+    ofstream saveFile(filename);
+    
+    if(saveFile.is_open())
+    {
+        saveFile << "These are the contents of the list" << endl;
+        for(int index = 0; index < dataSource.getSize(); index ++)
+        {
+            saveFile << "Food Title: " << dataSource.getFromIndex(index).getFoodName() << endl;
+        }
+    }
+    else
+    {
+        cerr << "File unavailable" << endl;
+    }
+    saveFile.close();
+}
